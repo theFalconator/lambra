12fdf32163845f5ef259541df9b3a677dcfea50d
theFalconator <falcon.nick@gmail.com>
2017-12-04 17:28:08 -0600

    init commit

---
+++ src/main/java/com/nfalcon/lambra/LambraApplication.java
@@ -0,0 +1,12 @@
+package com.nfalcon.lambra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LambraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LambraApplication.class, args);
+	}
+}
